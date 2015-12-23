a8efe8c31c56358da6295242eaecf7f14387cdfd
Pandora290 <yanziyi290@163.com>
2015-12-23 14:02:33 +0800

    add joystick
    add joystick

--- frameworks/runtime-src/Classes/AppDelegate.cpp
+++ frameworks/runtime-src/Classes/AppDelegate.cpp
@@ -3,6 +3,7 @@
 #include "SimpleAudioEngine.h"
 #include "cocos2d.h"
 #include "lua_module_register.h"
+#include "HelloWorldScene.h"
 
 #if (CC_TARGET_PLATFORM != CC_PLATFORM_LINUX)
 #include "ide-support/CodeIDESupport.h"
@@ -17,6 +18,10 @@ using namespace CocosDenshion;
 
 USING_NS_CC;
 using namespace std;
+static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
 
 AppDelegate::AppDelegate()
 {
@@ -53,8 +58,43 @@ static int register_all_packages()
 
 bool AppDelegate::applicationDidFinishLaunching()
 {
+	std::vector<std::string> searchPaths = CCFileUtils::getInstance()->getSearchPaths();
+	searchPaths.push_back("frameworks/runtime-src/Resources");
+	CCFileUtils::getInstance()->setSearchPaths(searchPaths);
+	auto director = Director::getInstance();
+	auto glview = director->getOpenGLView();
+	if (!glview) {
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
+		glview = GLViewImpl::createWithRect("MyTest", Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
+#else
+		glview = GLViewImpl::create("MyTest");
+#endif
+		director->setOpenGLView(glview);
+	}
+
+	// turn on display FPS
+	director->setDisplayStats(true);
     // set default FPS
-    Director::getInstance()->setAnimationInterval(1.0 / 60.0f);
+	director->setAnimationInterval(1.0 / 60.0f);
+
+	// Set the design resolution
+	glview->setDesignResolutionSize(designResolutionSize.width, designResolutionSize.height, ResolutionPolicy::NO_BORDER);
+	Size frameSize = glview->getFrameSize();
+	// if the frame's height is larger than the height of medium size.
+	if (frameSize.height > mediumResolutionSize.height)
+	{
+		director->setContentScaleFactor(MIN(largeResolutionSize.height / designResolutionSize.height, largeResolutionSize.width / designResolutionSize.width));
+	}
+	// if the frame's height is larger than the height of small size.
+	else if (frameSize.height > smallResolutionSize.height)
+	{
+		director->setContentScaleFactor(MIN(mediumResolutionSize.height / designResolutionSize.height, mediumResolutionSize.width / designResolutionSize.width));
+	}
+	// if the frame's height is smaller than the height of medium size.
+	else
+	{
+		director->setContentScaleFactor(MIN(smallResolutionSize.height / designResolutionSize.height, smallResolutionSize.width / designResolutionSize.width));
+	}
 
     // register lua module
     auto engine = LuaEngine::getInstance();
@@ -64,24 +104,30 @@ bool AppDelegate::applicationDidFinishLaunching()
 
     register_all_packages();
 
-    LuaStack* stack = engine->getLuaStack();
-    stack->setXXTEAKeyAndSign("2dxLua", strlen("2dxLua"), "XXTEA", strlen("XXTEA"));
+	LuaStack* stack = engine->getLuaStack();
+	stack->setXXTEAKeyAndSign("2dxLua", strlen("2dxLua"), "XXTEA", strlen("XXTEA"));
 
-    //register custom function
-    //LuaStack* stack = engine->getLuaStack();
-    //register_custom_function(stack->getLuaState());
+	//register custom function
+	//LuaStack* stack = engine->getLuaStack();
+	//register_custom_function(stack->getLuaState());
 
 #if (COCOS2D_DEBUG > 0) && (CC_CODE_IDE_DEBUG_SUPPORT > 0)
-    // NOTE:Please don't remove this call if you want to debug with Cocos Code IDE
-    auto runtimeEngine = RuntimeEngine::getInstance();
-    runtimeEngine->addRuntime(RuntimeLuaImpl::create(), kRuntimeEngineLua);
-    runtimeEngine->start();
+	// NOTE:Please don't remove this call if you want to debug with Cocos Code IDE
+	auto runtimeEngine = RuntimeEngine::getInstance();
+	runtimeEngine->addRuntime(RuntimeLuaImpl::create(), kRuntimeEngineLua);
+	runtimeEngine->start();
 #else
-    if (engine->executeScriptFile("src/main.lua"))
-    {
-        return false;
-    }
+	if (engine->executeScriptFile("src/main.lua"))
+	{
+		return false;
+	}
 #endif
+
+	// create a scene. it's an autorelease object
+	auto scene = HelloWorld::createScene();
+
+	// run
+	director->runWithScene(scene);
 
     return true;
 }
